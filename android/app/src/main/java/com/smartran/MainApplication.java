589fa357c906df69f6da1fe8405655ddf7545946
imran-blueravine <imran.ali@blue-ravine.com>
2018-09-15 12:52:28 +0530

    committed home screen by adding snackbar
    committed home screen by adding snackbar

--- android/app/src/main/java/com/smartran/MainApplication.java
+++ android/app/src/main/java/com/smartran/MainApplication.java
@@ -3,6 +3,7 @@ package com.smartran;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.someone.sendsms.SendSMSPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SnackbarPackage(),
             new SendSMSPackage(),
             new VectorIconsPackage(),
             new MapsPackage()
